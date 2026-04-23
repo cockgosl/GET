38c9ff1d27c92795b6b69a34598ba3e19ab4485a
Nikolaev Dmitriy <Igra.nikolaev@gmail.com>
2026-04-23 13:14:04 +0300

    sad

---
+++ get-dac/mcp4725-sin.py
@@ -0,0 +1,18 @@
+import signal_generator as sg
+from mcp4725_driver import MCP4725
+from time import time
+
+
+amplitude = 3.2
+signal_frequency = 10
+sampling_frequency = 1000
+
+
+if __name__ == "__main__":
+    try:
+        dac = MCP4725(5)
+        while True:
+            dac.set_voltage(amplitude * sg.get_sin_wave_amplitude(signal_frequency, time()))
+            sg.wait_for_sampling_period(sampling_frequency)
+    finally:
+        dac.deinit()
---
+++ get-dac/pwm-sin.py
@@ -0,0 +1,18 @@
+import signal_generator as sg
+from pwm_dac import PWM_DAC
+from time import time
+
+
+amplitude = 3.2
+signal_frequency = 10
+sampling_frequency = 1000
+
+
+if __name__ == "__main__":
+    try:
+        dac = PWM_DAC(12, 500, 3.2)
+        while True:
+            dac.set_voltage(amplitude * sg.get_sin_wave_amplitude(signal_frequency, time()))
+            sg.wait_for_sampling_period(sampling_frequency)
+    finally:
+        dac.deinit()

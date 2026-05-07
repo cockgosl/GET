72ebd97b78b2615bcea6d8a6f5a6040f2639a7aa
Nikolaev Dmitriy <Igra.nikolaev@gmail.com>
2026-05-07 12:35:32 +0300

    asd

--- get-adc/mcp3021_driver.py
+++ get-adc/mcp3021_driver.py
@@ -14,24 +14,36 @@ class MCP3021:
 
     def get_number(self):
         data = self.bus.read_word_data(self.address, 0)
+
         lower_data_byte = data >> 8
         upper_data_byte = data & 0xFF
+
         number = (upper_data_byte << 6) | (lower_data_byte >> 2)
+
         if self.verbose:
-            print(f"Принятые данные: {data}, Старший байт: {upper_data_byte:x}, Младший байт: {lower_data_byte:x}, Число: {number}")
+            print(
+                f"Принятые данные: {data}, "
+                f"Старший байт: {upper_data_byte:x}, "
+                f"Младший байт: {lower_data_byte:x}, "
+                f"Число: {number}"
+            )
+
         return number
 
     def get_voltage(self):
         number = self.get_number()
-        return (number / 1023.0) * self.dynamic_range
+        voltage = (number / 1023.0) * self.dynamic_range
+        return voltage
 
 
 if __name__ == "__main__":
+    adc = MCP3021(dynamic_range=5.0)
+
     try:
-        adc = MCP3021(dynamic_range=5.0)
         while True:
             voltage = adc.get_voltage()
             print(f"Напряжение: {voltage:.3f} В")
             time.sleep(1)
+
     finally:
         adc.deinit()

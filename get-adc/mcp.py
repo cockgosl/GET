ac85a791bc82ee2d9443743488105226ce54e8ff
Nikolaev Dmitriy <Igra.nikolaev@gmail.com>
2026-05-07 12:30:28 +0300

    s

--- get-adc/mcp.py
+++ get-adc/mcp.py
@@ -1,7 +1,7 @@
 import time
 import matplotlib.pyplot as plt
 
-from mcp3021 import MCP3021
+from mcp3021_driver import MCP3021
 
 
 DYNAMIC_RANGE = 5.0
@@ -10,9 +10,9 @@ DURATION = 3.0
 
 adc = MCP3021(dynamic_range=DYNAMIC_RANGE)
 
-voltages = []
-times = []
-measurement_periods = []
+voltage_values = []
+time_values = []
+measurement_durations = []
 
 try:
     start_time = time.time()
@@ -25,32 +25,31 @@ try:
         measurement_end_time = time.time()
 
         current_time = measurement_end_time - start_time
-        measurement_period = measurement_end_time - measurement_start_time
+        measurement_duration = measurement_end_time - measurement_start_time
 
-        times.append(current_time)
-        voltages.append(voltage)
-        measurement_periods.append(measurement_period)
+        voltage_values.append(voltage)
+        time_values.append(current_time)
+        measurement_durations.append(measurement_duration)
 
         print(
             f"t = {current_time:.3f} с, "
             f"U = {voltage:.3f} В, "
-            f"T = {measurement_period:.6f} с"
+            f"T = {measurement_duration:.6f} с"
         )
 
-    plt.figure()
-    plt.plot(times, voltages)
-    plt.title("График зависимости напряжения на входе АЦП от времени")
+    plt.figure(figsize=(10, 6))
+    plt.plot(time_values, voltage_values)
+    plt.title("График зависимости напряжения на входе MCP3021 от времени")
     plt.xlabel("Время, с")
     plt.ylabel("Напряжение, В")
+    plt.xlim(0, max(time_values))
+    plt.ylim(0, DYNAMIC_RANGE)
     plt.grid(True)
 
-    plt.figure()
-    plt.hist(measurement_periods, bins=20)
-    plt.title(
-        "Распределение периодов дискретизации измерений\n"
-        "по времени на одно измерение"
-    )
-    plt.xlabel("Период измерения, с")
+    plt.figure(figsize=(10, 6))
+    plt.hist(measurement_durations, bins=20)
+    plt.title("Распределение количества измерений по их продолжительности")
+    plt.xlabel("Продолжительность измерения, с")
     plt.ylabel("Количество измерений")
     plt.grid(True)
 

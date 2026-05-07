4dac5992fb7ceb133938586b9c99e97ea8e81230
Nikolaev Dmitriy <Igra.nikolaev@gmail.com>
2026-05-07 12:03:47 +0300

    sad

--- get-adc/r2r_adc.py
+++ get-adc/r2r_adc.py
@@ -3,7 +3,7 @@ import time
 
 
 class R2R_ADC:
-    def __init__(self, dynamic_range, compare_time=0.01, verbose=False):
+    def __init__(self, dynamic_range, compare_time=0.001, verbose=False):
         self.dynamic_range = dynamic_range
         self.verbose = verbose
         self.compare_time = compare_time
@@ -30,7 +30,7 @@ class R2R_ADC:
         GPIO.output(self.bits_gpio, bits)
 
         if self.verbose:
-            print(f"Число: {number}, биты: {bits}")
+            print(f"[DAC] {number} -> {bits}")
 
     def sequential_counting_adc(self):
         for number in range(256):
@@ -38,23 +38,29 @@ class R2R_ADC:
             time.sleep(self.compare_time)
 
             if GPIO.input(self.comp_gpio) == 1:
+                # Оставляем верхнюю точку пилы на экране чуть дольше
+                time.sleep(self.compare_time * 5)
+
+                # Сбрасываем ЦАП в 0, чтобы начался новый зуб пилы
+                self.number_to_dac(0)
+                time.sleep(self.compare_time * 2)
+
                 return number
 
+        self.number_to_dac(0)
         return 255
 
     def get_sc_voltage(self):
         number = self.sequential_counting_adc()
-        voltage = (number / 255) * self.dynamic_range
-        return voltage
+        return (number / 255.0) * self.dynamic_range
 
 
 if __name__ == "__main__":
-    adc = R2R_ADC(dynamic_range=3.3)
+    adc = R2R_ADC(dynamic_range=3.3, compare_time=0.001)
 
     try:
         while True:
-            voltage = adc.get_sc_voltage()
-            print(f"Напряжение: {voltage:.3f} В")
+            adc.sequential_counting_adc()
 
     finally:
         adc.deinit()
